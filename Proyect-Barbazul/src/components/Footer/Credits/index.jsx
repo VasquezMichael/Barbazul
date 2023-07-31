637392039a87ab134599a2cde4f1edf062f29071
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-07-31 20:57:24 -0300

    Probando cambios en rama

--- Proyect-Barbazul/src/components/Footer/Credits/index.jsx
+++ Proyect-Barbazul/src/components/Footer/Credits/index.jsx
@@ -6,6 +6,12 @@ const Credits = () => {
     <Container>
       <Link to="https://www.linkedin.com/feed/" target="_blank">
         <p>Diseñado y programado por Vasquez Michael</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+          voluptatem fuga quis qui est recusandae quidem distinctio nesciunt
+          numquam repellat ea modi corporis iusto similique enim reiciendis!
+          Laudantium, dicta repellendus?
+        </p>
       </Link>
     </Container>
   );
